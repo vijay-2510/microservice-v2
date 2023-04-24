b34bc11243ba4b5676aea47829eb200104b98d49
Vijay Pandey <kumarvijay2510@gmail.com>
2023-04-24 14:55:10 +0530

    Added Zipkin distribured traceing
    Updated Currency Exchange Service to use zipkin container on GCP for tracing

--- currency-exchange-service/src/main/java/com/vijay/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/vijay/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.vijay.microservices.currencyexchangeservice.controller;
 
 import com.vijay.microservices.currencyexchangeservice.bean.CurrencyExchange;
 import com.vijay.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
     @Autowired
     private Environment environment;
 
@@ -21,8 +24,8 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
+        logger.info("retrieveExchangeValue called with {} to {}",from,to);
 
-       // CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(79));
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from,to);
         if(currencyExchange == null){
             throw new RuntimeException("Unable to find data for "+from+" to "+to);
