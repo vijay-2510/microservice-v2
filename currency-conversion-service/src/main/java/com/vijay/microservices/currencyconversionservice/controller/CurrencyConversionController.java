032fb576617ec8a7c001a74d60a333127ea3eff8
Vijay Pandey <kumarvijay2510@gmail.com>
2023-04-24 15:33:34 +0530

    Added changes for tracing
    Updated changes for tracing config to api gateway and currency conversion service

--- currency-conversion-service/src/main/java/com/vijay/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/vijay/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -2,7 +2,10 @@ package com.vijay.microservices.currencyconversionservice.controller;
 
 import com.vijay.microservices.currencyconversionservice.bean.CurrencyConversion;
 import com.vijay.microservices.currencyconversionservice.proxy.CurrencyExchangeProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,16 +18,21 @@ import java.util.HashMap;
 @RestController
 public class CurrencyConversionController {
 
+    Logger logger = LoggerFactory.getLogger(CurrencyConversionController.class);
+
     @Autowired
     private CurrencyExchangeProxy currencyExchangeProxy;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
-
+        logger.info("calculateCurrencyConversion from {} to {} quantity {}",from,to,quantity);
         HashMap<String,String> uriVariables = new HashMap<>();
         uriVariables.put("from",from);
         uriVariables.put("to",to);
-        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+        ResponseEntity<CurrencyConversion> responseEntity = restTemplate.getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
         CurrencyConversion currencyConversion = responseEntity.getBody();
         return new CurrencyConversion(currencyConversion.getId(),from,to, currencyConversion.getConversionMultiple(),quantity
                 ,quantity.multiply(currencyConversion.getConversionMultiple()),currencyConversion.getEnvironment());
